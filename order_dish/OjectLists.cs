c7ed6c7f842dd30efc68566c6e1c291bc1190e22
mistic1265 <cebotavictor14@gmail.com>
2025-02-14 13:28:26 +0200

    add order_dish

---
+++ order_dish/OjectLists.cs
@@ -0,0 +1,191 @@
+﻿using order_dish;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ObjectLists
+{
+    class IngredientList
+    {
+        public List<Ingredient>? ingredients { get; private set; }
+        public float dishPrice { get; private set; }
+
+        public IngredientList()
+        { 
+            ingredients = new List<Ingredient>();
+        }
+        public void add(Ingredient ingredient)
+        {
+            if (ingredient == null) throw new NullReferenceException("Ingredient is null");
+            ingredients?.Add(ingredient);
+        }
+    }
+
+
+
+    class CookList
+    {
+        public List<Cook> cooks;
+        public int CooksCount
+        {
+            get
+            {
+                return cooks.Count;
+            }
+        }
+
+        public CookList()
+        {
+            cooks = new List<Cook>();
+        }
+
+        public void addCookToList(Cook cook)
+        {
+            if (cook == null) throw new NullReferenceException("Cook is null");
+            cooks.Add(cook);
+        }
+
+        public bool addDishesToSortedCooks(Dish dish)
+        {
+            cooks.Sort((x, y) => x.numberOfDishesAssigned.CompareTo(y.numberOfDishesAssigned));//This method sort implements QuickSort.On average, this method is an O(n log n) operation, where n is Count; in the worst case it is an O(n ^ 2) operation.
+            Cook cook = cooks[0];
+            if (cook.numberOfDishesAssigned >= 5) { return false; }
+            cook.addDishesToCook(dish);
+            return true;
+        }
+
+        public Cook returnCurrectCookWithNrOfDishes()
+        {
+            cooks.Sort((x, y) => -x.numberOfDishesAssigned.CompareTo(y.numberOfDishesAssigned));
+            return cooks[0];
+        }
+
+        public int calculateTotalCookTime()
+        {
+            int totalCookTime = 0;
+            foreach (Cook cook in cooks)
+            {
+                foreach (Dish dish in cook.returnDisheList())
+                {
+                    totalCookTime += dish.minutesToFinishDish;
+                }
+            }
+            return totalCookTime;
+        }
+
+        public float calculateTotalDishPrice()
+        {
+            int totalDishPrice = 0;
+            foreach (var cook in cooks)
+            {
+                foreach (var dish in cook.returnDisheList())
+                {
+                    totalDishPrice += (int)dish.dishPrice;
+                }
+            }
+            return totalDishPrice;
+        }
+    }
+
+    class DishIngredientList
+        {
+        private Dictionary<string, IngredientList> _ingredients;
+
+        public DishIngredientList()
+        {
+            _ingredients = new Dictionary<string, IngredientList>();
+        }
+
+        public void set_ingredients(string name, IngredientList i)
+        {
+            if (name == null) throw new NullReferenceException("The ingredients list is null");
+            _ingredients[name] = i;
+        }
+        public IngredientList getCurrentIngredientsList(string name)
+        {
+            if (_ingredients.ContainsKey(name))
+                return _ingredients[name];
+            else throw new Exception("this name is nonexistent");
+        }
+
+        public float calculateTotalPrice(string name)
+        {
+            IngredientList dishLstget = getCurrentIngredientsList(name);
+            float total = 0;
+            foreach (var ingredient in dishLstget.ingredients)
+            {
+                total += ingredient.ingredinetPrice;
+            }
+            return total;
+        }
+
+    }
+
+    class objLists
+    {
+        public static DishIngredientList ReturnIngredentslists()
+        {
+
+            //here is a list with all ingresients, after this list is created, the owner of this list can find the best choice for the dish, as an self with ingredients.
+
+            //Eggs, milk/cream , salt, pepper, optional: cheese, veggies (onions, spinach) - order dish 
+            IngredientList Eggs = new IngredientList();
+            Eggs.add(new Ingredient { ingredientName = "Eggs", ingredinetPrice = 7 });
+            Eggs.add(new Ingredient { ingredientName = "milk/cream", ingredinetPrice = 8 });
+            Eggs.add(new Ingredient { ingredientName = "salt", ingredinetPrice = 3 });
+            Eggs.add(new Ingredient { ingredientName = "oil", ingredinetPrice = 5 });
+
+            //Pasta, canned tomatoes, garlic, onion, olive oil, herbs (basil, oregano)
+            IngredientList Pasta = new IngredientList();
+            Pasta.add(new Ingredient { ingredientName = "Pasta", ingredinetPrice = 9 });
+            Pasta.add(new Ingredient { ingredientName = "canned tomatoes", ingredinetPrice = 10 });
+            Pasta.add(new Ingredient { ingredientName = "garlic", ingredinetPrice = 11 });
+            Pasta.add(new Ingredient { ingredientName = "onion", ingredinetPrice = 12 });
+            Pasta.add(new Ingredient { ingredientName = "olive oil", ingredinetPrice = 13 });
+            Pasta.add(new Ingredient { ingredientName = "herbs", ingredinetPrice = 12 });
+
+            //Lentil Soup: Lentils, carrots, onion, celery, vegetable broth, herbs (thyme, bay leaf)
+            IngredientList LentilSoup = new IngredientList();
+            LentilSoup.add(new Ingredient { ingredientName = "Lentils", ingredinetPrice = 9 });
+            LentilSoup.add(new Ingredient { ingredientName = "carrots", ingredinetPrice = 10 });
+            LentilSoup.add(Pasta.ingredients?.Find(x => x.ingredientName == "onion") ?? new Ingredient { ingredientName = "onion", ingredinetPrice = 12 }); // in pasta we already have onion ingredinet
+            LentilSoup.add(new Ingredient { ingredientName = "celery", ingredinetPrice = 8 });
+            LentilSoup.add(new Ingredient { ingredientName = "vegetable broth", ingredinetPrice = 12 });
+            LentilSoup.add(new Ingredient { ingredientName = "herbs", ingredinetPrice = 19 });
+
+            //Grilled Cheese: Bread, cheese, butter
+            IngredientList GrilledCheese = new IngredientList();
+            GrilledCheese.add(new Ingredient { ingredientName = "Bread", ingredinetPrice = 9 });
+            GrilledCheese.add(new Ingredient { ingredientName = "Cheese", ingredinetPrice = 10 });
+            GrilledCheese.add(new Ingredient { ingredientName = "butter", ingredinetPrice = 12 });
+
+
+            //Tuna Salad Sandwich: Canned tuna, mayonnaise, onion, celery, lettuce, bread
+            IngredientList TunaSaladSandwich = new IngredientList();
+            GrilledCheese.add(new Ingredient { ingredientName = "Canned tuna", ingredinetPrice = 10 });
+            GrilledCheese.add(new Ingredient { ingredientName = "mayonnaise", ingredinetPrice = 11 });
+            GrilledCheese.add(Pasta.ingredients?.Find(x => x.ingredientName == "onion") ?? new Ingredient { ingredientName = "onion", ingredinetPrice = 12 }); // in pasta we already have onion ingredinet
+            GrilledCheese.add(new Ingredient { ingredientName = "celery", ingredinetPrice = 11 });
+            GrilledCheese.add(new Ingredient { ingredientName = "lettuce", ingredinetPrice = 11 });
+            GrilledCheese.add(new Ingredient { ingredientName = "bread", ingredinetPrice = 11 });
+
+            DishIngredientList ingredientList = new DishIngredientList();
+            ingredientList.set_ingredients("Eggs", Eggs);
+            ingredientList.set_ingredients("Pasta", Pasta);
+            ingredientList.set_ingredients("LentilSoup", LentilSoup);
+            ingredientList.set_ingredients("GrilledCheese", GrilledCheese);
+            ingredientList.set_ingredients("TunaSaladSandwich", TunaSaladSandwich);
+
+
+            return ingredientList;
+
+        }
+
+
+    }
+
+}
---
+++ order_dish/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Quic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Channels;
+using ObjectLists;
+
+namespace order_dish
+{
+
+
+    class Dish
+    {
+        private IngredientList _ingredients;
+
+        public string dishName { get; }
+        public string dishDescription { get; }
+        public int minutesToFinishDish { get; }
+        public float dishPrice { get; private set; }
+
+        public Dish(string name, string description, IngredientList ingredients, int minutes, float price)
+        {
+            _ingredients = ingredients;
+            dishName = name;
+            dishDescription = description;
+            minutesToFinishDish = minutes;
+            dishPrice = price;
+        }
+    }
+
+    class Ingredient
+    {
+        public string ingredientName;
+        public int ingredinetPrice;
+
+        public Ingredient()
+        {
+            ingredientName = "";
+            ingredinetPrice = 0;
+        }
+    }
+
+ 
+
+    class Cook
+    {
+        private List<Dish> _dishesAssigendToCook;
+        public string cookName;
+        public int numberOfDishesAssigned
+        {
+            get
+            {
+                return _dishesAssigendToCook.Count;
+            }
+            set { }
+        }
+
+        public Cook(string name)
+        {
+            cookName = name;
+            _dishesAssigendToCook = new List<Dish>();
+            numberOfDishesAssigned = 0;
+        }
+
+        public bool addDishesToCook(Dish dish)
+        {
+            if (dish == null) throw new NullReferenceException("Dish is null");
+            _dishesAssigendToCook.Add(dish);
+            return true;
+        }
+        
+        public List<Dish> returnDisheList()
+        {
+            return _dishesAssigendToCook;
+        }
+
+    }
+    
+
+    
+    internal class Program
+    {     
+
+
+        public static CookList ReturnCookList()
+        {
+            CookList cookList = new CookList();
+            Cook cook = new Cook("John");
+            cookList.addCookToList(cook);
+            cook = new Cook("Larry");
+            cookList.addCookToList(cook);
+            return cookList;
+        }
+
+
+        public static int Rand()
+        {
+            Random random = new Random();
+            int rand_number = random.Next(10, 30);
+            return rand_number;
+        }
+
+        public static void Menu()
+        {
+            //here is the list of all dishes and their appropriate descriptions 
+            string[,] dishes = new string[10, 2];
+            dishes[0, 0] = "Scrambled Eggs";
+            dishes[0, 1] = "Scrambled eggs are a quick and easy breakfast dish made by mixing eggs, milk/cream, salt, and pepper. Optional ingredients include cheese and veggies like onions and spinach";
+            dishes[1, 0] = "Pasta with Tomato Sauce";
+            dishes[1, 1] = "Pasta with tomato sauce is a classic Italian dish made by cooking pasta and topping it with a sauce made from canned tomatoes, garlic, onion, olive oil, and herbs like basil and oregano.";
+            dishes[2, 0] = "Lentil Soup";
+            dishes[2, 1] = "Lentil soup is a hearty and nutritious dish made by cooking lentils with carrots, onion, celery, vegetable broth, and herbs like thyme and bay leaf.";
+            dishes[3, 0] = "Grilled Cheese";
+            dishes[3, 1] = "Grilled cheese is a simple and delicious sandwich made by grilling bread with cheese and butter";
+            dishes[4, 0] = "Tuna Salad Sandwich";
+            dishes[4, 1] = "Tuna salad sandwich is a tasty and satisfying sandwich made by mixing canned tuna with mayonnaise, onion, and celery, and serving it on bread with lettuce.";
+            DishIngredientList newList = objLists.ReturnIngredentslists();
+
+            string choice;
+            Console.WriteLine("Create your order by selecting the dishes by their respective name and your dish will be assigned to the appropriate cook");
+            Console.WriteLine($"Scrambled Eggs({newList.calculateTotalPrice("Eggs") + (newList.calculateTotalPrice("Eggs") * 20)/100} mdl). Description - {dishes[0,1]}. \nInput name - Eggs\n");
+            Console.WriteLine($"Pasta with Tomato Sauce({newList.calculateTotalPrice("Pasta") + (newList.calculateTotalPrice("Eggs") * 20) / 100} mdl). Description - {dishes[0,1]}. \nInput name - Pasta\n");
+            Console.WriteLine($"Lentil Soup({newList.calculateTotalPrice("LentilSoup") + (newList.calculateTotalPrice("LentilSoup") * 20) / 100} mdl). Description - {dishes[0, 1]}.  \nInput name -  LentilSoup\n");
+            Console.WriteLine($"Grilled Cheese({newList.calculateTotalPrice("GrilledCheese") + (newList.calculateTotalPrice("GrilledCheese") * 20) / 100} mdl). Description - {dishes[0, 1]}.  \nInput name -  GrilledCheese\n");
+            Console.WriteLine($"Tuna Salad Sandwich({newList.calculateTotalPrice("GrilledCheese") + (newList.calculateTotalPrice("GrilledCheese") * 20) / 100} mdl). Description - {dishes[0, 1]}.  \nInput name - TunaSaladSandwich\n");
+
+            Console.WriteLine("How many dishes do you want to order ? ");
+            string nrOfDishes = Console.ReadLine() ?? "0";
+            if (int.TryParse(nrOfDishes, out int nr))
+            {
+                DishIngredientList list = objLists.ReturnIngredentslists();
+                Dish dish;
+                Cook cook;
+                CookList cookList = ReturnCookList();
+                int i = 0;
+
+                while (i < nr)
+                {
+                    Console.WriteLine("Enter the dish name: ");
+                    choice = Convert.ToString(Console.ReadLine()) ?? "";
+                    try
+                    {
+                        dish = new Dish(dishes[0, 0], dishes[0, 1], list.getCurrentIngredientsList(choice), Rand(), list.calculateTotalPrice(choice));
+                        bool isAdded = cookList.addDishesToSortedCooks(dish);
+                        if (isAdded == false) { Console.WriteLine("No cooks available"); break; }
+                        cook = cookList.returnCurrectCookWithNrOfDishes();
+                        Console.WriteLine($"Dish assigned to cook {cook.cookName} the time estimated for {choice} is {dish.minutesToFinishDish} minutes. His order lists include {cook.numberOfDishesAssigned}");
+                    }
+                    catch (Exception ex) { Console.WriteLine("Incorrect dish name, try again!"); continue; }
+                    i++;
+                }
+
+              
+                TimeSpan estimatedTime = new TimeSpan(0, cookList.calculateTotalCookTime(), 0);
+
+                Console.WriteLine($"Estimated time is {estimatedTime}");
+                Console.WriteLine($"Total acumulated price is {cookList.calculateTotalDishPrice() + (cookList.calculateTotalDishPrice() * 20) / 100} mdl");
+            }
+            else
+            {
+                Console.WriteLine("Please Input a valid number"); return;
+            }
+           
+
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Menu();
+
+        }
+    }
+}
